b3e1d648c5e60cc172e9e5abe69edab239328093
Laura <laurawolfart@gmail.com>
2025-03-12 22:51:09 -0400

    disables onscroll animation for Safari

--- index.js
+++ index.js
@@ -1,50 +1,79 @@
-const config = {
-  root: document.body,
-  rootMargin: '-100px 0px',
-  threshold: [...Array(30).keys()].map(x => x / 29)
-};
+const detectBrowser = (() => { 
+    const userAgent = navigator.userAgent;
+    if (userAgent.indexOf("Edg") > -1) {
+        console.log("Microsoft Edge")
+        return "Microsoft Edge";
+    } else if (userAgent.indexOf("Chrome") > -1) {
+        console.log("Chrome")
+        return "Chrome";
+    } else if (userAgent.indexOf("Firefox") > -1) {
+        console.log("Firefox")
+        return "Firefox";
+    } else if (userAgent.indexOf("Safari") > -1) {
+        console.log("Safari")
+        return "Safari";
+    } else if (userAgent.indexOf("Opera") > -1) {
+        console.log("Opera")
+        return "Opera";
+    } else if (userAgent.indexOf("Trident") > -1 || userAgent.indexOf("MSIE") > -1) {
+        console.log("Internet Explorer")
+        return "Internet Explorer";
+    }
+
+    return "Unknown";
+})
+
+
+
+const isAnimated = (() => {
+    const hiddenElementsRight = document.querySelectorAll('.hiddenRight')
+    const hiddenElementsLeft = document.querySelectorAll('.hiddenLeft')
+    const hiddenElements = document.querySelectorAll('.hidden')
+    const browser = detectBrowser();
+    if (browser === "Safari")  {
+        hiddenElementsRight.forEach((el) => el.classList.add('showRight')) 
+        hiddenElementsLeft.forEach((el) => el.classList.add('showLeft'))
+        hiddenElements.forEach((el) => el.classList.add('show'))
+    } else {
+        hiddenElementsRight.forEach((el) => {
+            observerRight.observe(el)
+        } )
+        hiddenElementsLeft.forEach((el) => observerLeft.observe(el))
+        hiddenElements.forEach((el) => observer.observe(el))
+    }
+})
+
+
 
 const observerRight = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting) {
-            console.log("Show Right")
             entry.target.classList.add('showRight')
         } else {
             entry.target.classList.remove('showRight')
         }
     })
-}, config)
-
-const hiddenElementsRight = document.querySelectorAll('.hiddenRight')
-hiddenElementsRight.forEach((el) => observerRight.observe(el))
-
+})
 const observerLeft = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting) {
-            console.log("Show Left")
             entry.target.classList.add('showLeft')
         } else {
             entry.target.classList.remove('showLeft')
         }
     })
-}, config)
-
-const hiddenElementsLeft = document.querySelectorAll('.hiddenLeft')
-hiddenElementsLeft.forEach((el) => observerLeft.observe(el))
-
-
-
+})
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting) {
-            console.log("Show")
             entry.target.classList.add('show')
         } else {
             entry.target.classList.remove('show')
         }
     })
-}, config)
+})
 
-const hiddenElements = document.querySelectorAll('.hidden')
-hiddenElements.forEach((el) => observer.observe(el))
+
+isAnimated()
+
 
